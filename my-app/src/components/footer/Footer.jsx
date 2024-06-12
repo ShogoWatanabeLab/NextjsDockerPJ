d4bff5e25589f63da57c09c7179228c6ce5fa426
ShogoWatanabe <shogoxxx9@gmail.com>
2024-06-12 14:23:05 +0900

    [add] create Header, Footer components

---
+++ my-app/src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+    <footer className="bg-blue-600 text-white text-center p-4 mt-auto">
+        <p>© 2024 My Next.js App</p>
+    </footer>
+    );
+};
+
+export default Footer;
---
+++ my-app/src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Next.js Docker Project</h1>
+        </header>
+    )
+};
+
+export default Header;
