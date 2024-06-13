e80791da696343aba3206d360f4f39e05ec66409
ShogoWatanabe <shogoxxx9@gmail.com>
2024-06-13 13:58:19 +0900

    [fix] header

--- my-app/src/components/header/Nav.jsx
+++ my-app/src/components/header/Nav.jsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { NavLink } from "../index";
 
 const Nav = (props) => {
--- my-app/src/components/header/NavLink.jsx
+++ my-app/src/components/header/NavLink.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const NavLink = (props) => {
   return (
-    <Link href={props.url} className="ml-4 hover:underline">
+    <Link href={props.url} className="px-8 py-2 ml-4 border border-solid rounded-xl border-white hover:bg-gray-100 hover:border-gray-300">
       {props.title}
     </Link>
   );
